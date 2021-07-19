e0b604344492f2566ac59872f227d191ffdf6ae2
Emival Santana <emival.ceres@gmail.com>
2021-07-19 20:50:58 -0300

    Projeto criado

---
+++ src/test/java/com/cursomodelagem/CursomodelagemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursomodelagem;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomodelagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

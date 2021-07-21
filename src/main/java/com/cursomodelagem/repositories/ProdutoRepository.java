1e6d9ab73515649bc23a895313af8200eab4bd1b
Emival Santana <emival.ceres@gmail.com>
2021-07-21 20:44:21 -0300

    Produto e associação muitos para muitos

---
+++ src/main/java/com/cursomodelagem/repositories/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.cursomodelagem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cursomodelagem.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}

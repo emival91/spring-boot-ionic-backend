ca934c30952408b6759bf6185f3665754023a842
Emival Santana <emival.ceres@gmail.com>
2021-07-22 21:13:26 -0300

    Estado e cidade

---
+++ src/main/java/com/cursomodelagem/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package com.cursomodelagem.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cursomodelagem.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
